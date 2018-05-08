cf0d895a83251c4d3bfdc1eebab039375babbbf9
yunlzhang <zhangyunlong_fdzs@163.com>
2018-05-08 22:15:59 +0800

    add gettoken

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 
-
 function getPath(file){
     return require('./route/' + file);
 }
@@ -7,7 +6,8 @@ module.exports = function (app) {
         
     app.use('/signup', getPath('signup'));
     app.use('/signin', getPath('signin'));
-    app.use('/signout',getPath('signout'));    
+    app.use('/signout',getPath('signout'));
+    app.use('/gettoken',getPath('./gettoken'));    
     app.use('/get_user_info',getPath('getuserinfo'));
     app.use('/article',getPath('article'));
     app.use('/upload',getPath('upload'));
---
+++ server/routes/route/gettoken.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let router = express.Router();
+let qiniu  = require('qiniu');
+let config = require(process.cwd() + '/config/config');
+let accessKey = config.qiniu.accessKey;
+let secretKey = config.qiniu.secretKey;
+
+let options = {
+    scope: 'pict',
+};
+let putPolicy = new qiniu.rs.PutPolicy(options);
+
+router.get('/',function(req,res,next){
+    let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    let uploadToken=putPolicy.uploadToken(mac);
+    res.json({
+        code:200,
+        uptoken:uploadToken
+    })
+})
+module.exports = router;
